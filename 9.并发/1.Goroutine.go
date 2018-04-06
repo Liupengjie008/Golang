b79a7a6fcac19e6d6dd0d45028d8bf3326800e50
刘鹏杰 <liupengjie@liupengjiedeMacBook-Pro.local>
2018-04-06 13:03:09 +0800

    golang

--- 9.并发/1.Goroutine.go
+++ 9.并发/1.Goroutine.go
@@ -4,140 +4,151 @@ Golang 在语言层面对并发编程提供支持，一种类似协程，称作 
 
 事实上，入口函数 main 就以 goroutine 运行。另有与之配套的 channel 类型，用以实现 "以通讯来共享内存" 的 CSP 模式。
 
-go func() {
-    println("Hello, World!")
-}()
+package main
 
+import "time"
 
+func main() {
+	go func() {
+		println("Hello, World!")
+	}()
+	time.Sleep(1 * time.Second)
+}
+输出结果：
+
+Hello, World!
 调度器不能保证多个 goroutine 执行次序，且进程退出时不会等待它们结束。
 
 默认情况下，进程启动后仅允许一个系统线程服务于 goroutine。可使用环境变量或标准库函数 runtime.GOMAXPROCS 修改，让调度器用多个线程实现多核并行，而不仅仅是并发。
- 
+
 package main
 
 import (
-    "math"
-    "sync"
+	"math"
+	"sync"
 )
 
 func sum(id int) {
-    var x int64
-    for i := 0; i < math.MaxUint32; i++ {
-        x += int64(i)
-    }
+	var x int64
+	for i := 0; i < math.MaxUint32; i++ {
+		x += int64(i)
+	}
 
-    println(id, x)
+	println(id, x)
 }
 
 func main() {
-    wg := new(sync.WaitGroup)
-    wg.Add(2)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
 
-    for i := 0; i < 2; i++ {
-        go func(id int) {
-            defer wg.Done()
-            sum(id) 
-        }(i)
-    }
-    
-    wg.Wait() 
+	for i := 0; i < 2; i++ {
+		go func(id int) {
+			defer wg.Done()
+			sum(id)
+		}(i)
+	}
+
+	wg.Wait()
 }
+命令行输入：
 
-输出:
+go build main.go 
 
-$ go build -o test
-
-$ time -p ./test
-
-0 9223372030412324865
-1 9223372030412324865
-
-
-real   7.70     // 程序开始到结束时间差 (  CPU 时间)
-user   7.66     // 用户态所使用 CPU 时间片 (多核累加) 
-sys    0.01     // 内核态所使用 CPU 时间片
- 
-$ GOMAXPROCS=2 time -p ./test
-
+time -p ./main
+输出结果：
 
 0 9223372030412324865
 1 9223372030412324865
+real 1.92	 // 程序开始到结束时间差 (  CPU 时间)
+user 3.80	// 用户态所使用 CPU 时间片 (多核累加) 
+sys 0.01	// 内核态所使用 CPU 时间片
+命令行输入：
 
-real 4.18
-user 7.61       // 虽然总时间差不多，但由 2 个核并行，real 时间自然少了许多。 
-sys 0.02
+GOMAXPROCS=8 time -p ./main
+输出结果：
 
-
+1 9223372030412324865
+0 9223372030412324865
+real         1.89
+user         3.76	// 虽然总时间差不多，但由 2 个核并行，real 时间自然少了许多。 
+sys          0.00
 调用 runtime.Goexit 将立即终止当前 goroutine 执行，调度器确保所有已注册 defer 延迟调用被执行。
 
 package main
 
 import (
-    "runtime"
-    "sync"
+	"runtime"
+	"sync"
 )
 
 func main() {
-    wg := new(sync.WaitGroup)
-    wg.Add(1)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
 
-    go func() {
-        defer wg.Done()
-        defer println("A.defer")
+	go func() {
+		defer wg.Done()
+		defer println("A.defer")
 
-        func() {
-            defer println("B.defer")
-            runtime.Goexit()     // 终止当前 goroutine
-            println("B")          // 不会执行 
-        }()
+		func() {
+			defer println("B.defer")
+			runtime.Goexit() // 终止当前 goroutine
+			println("B")     // 不会执行
+		}()
 
-        println("A")            // 不会执行
-    }()
+		println("A") // 不会执行
+	}()
 
-    wg.Wait() 
+	wg.Wait()
 }
- 
-输出:
+输出结果:
+
 B.defer
 A.defer
-
-
 和协程 yield 作用类似，Gosched 让出底层线程，将当前 goroutine 暂停，放回队列等待下次被调度执行。
 
 package main
 
 import (
-    "runtime"
-    "sync"
+	"runtime"
+	"sync"
 )
 
 func main() {
-    wg := new(sync.WaitGroup)
-    wg.Add(2)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		for i := 0; i < 6; i++ {
+			println(i)
+			runtime.Gosched()
+		}
+		defer wg.Done()
+	}()
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			println("Hello, World!")
+		}()
+	}
 
-        for i := 0; i < 6; i++ {
-            println(i)
-            if i == 3 { runtime.Gosched() }
-        }
-    }()
-
-    go func() {
-        defer wg.Done()
-        println("Hello, World!")
-    }()
-    
-    wg.Wait() 
+	wg.Wait()
 }
+输出结果:
 
-输出:
-$ go run main.go
+Hello, World!
+Hello, World!
 0
 1
+Hello, World!
+Hello, World!
+Hello, World!
 2
 3
-Hello, World!
 4
 5
+Hello, World!
+
+每次输出结果都不一样
+runtime.Gosched()用于让出CPU时间片。这就像跑接力赛，A跑了一会碰到代码runtime.Gosched()就把接力棒交给B了，A歇着了，B继续跑。
+
